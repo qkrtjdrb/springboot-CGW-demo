bf0e128d51f38bbd14d021bb2df0af6cd4b7e3e1
qkrtjdrb <qkrtjdrb321@naver.com>
2024-12-18 11:54:53 +0900

    first commit

---
+++ src/main/java/com/example/springboot_CGW_demo/SpringbootCgwDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_CGW_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCgwDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCgwDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_CGW_demo/SpringbootCgwDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_CGW_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCgwDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
